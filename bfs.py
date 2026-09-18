b0b707bf310dc6ee3f0d2ebde1057347c94d9578
Ishita <ishitaagrawal978@gmail.com>
2026-09-19 00:52:41 +0500

    program

--- bfs.py
+++
@@ -1,7 +0,0 @@
-"""Run the water jug problem with Breadth First Search."""
-
-from water_jug_problem import breadth_first_search, print_solution
-
-
-if __name__ == "__main__":
-    print_solution("BFS", breadth_first_search(4, 3, 2))
--- waterjugproblem/dfs.py
+++
@@ -1,7 +0,0 @@
-"""Run the water jug problem with Depth First Search."""
-
-from water_jug_problem import depth_first_search, print_solution
-
-
-if __name__ == "__main__":
-    print_solution("DFS", depth_first_search(4, 3, 2))
